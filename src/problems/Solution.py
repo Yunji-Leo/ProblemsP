ee20050e6647406a7c3ce5e815495618c5afcc6b
Leo Zhong <yunji@uber.com>
2019-07-20 15:13:07 -0700

    3 sum

--- src/problems/Solution.py
+++ src/problems/Solution.py
@@ -386,3 +386,27 @@ class Solution:
             if left[i] != right[i]:
                 return left[:i]
         return left[:minLength]
+
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        result = []
+        for i in range(len(nums) - 2):
+            if i == 0 or nums[i] != nums[i - 1]:
+                left = i + 1
+                right = len(nums) - 1
+                while left < right:
+                    sum = nums[i] + nums[left] + nums[right]
+                    if sum > 0:
+                        right = right - 1
+                    elif sum < 0:
+                        left = left + 1
+                    else:
+                        result.append((nums[i], nums[left], nums[right]))
+                        while left < right and nums[left] == nums[left + 1]:
+                            left = left + 1
+                        while left < right and nums[right] == nums[right - 1]:
+                            right = right - 1
+                        left = left + 1
+                        right = right - 1
+
+        return result
