9ce80cbd4e66442fa7cf8a73a89e972559cdf647
Leo Zhong <yunji@uber.com>
2019-07-21 13:58:58 -0700

    two eazy

--- src/problems/Solution.py
+++ src/problems/Solution.py
@@ -494,3 +494,35 @@ class Solution:
             second = second.next
         second.next = second.next.next
         return dummy.next
+
+    def isValid(self, s: str) -> bool:
+        stack = []
+        for i in range(len(s)):
+            if s[i] == '(':
+                stack.append(')')
+            elif s[i] == '[':
+                stack.append(']')
+            elif s[i] == '{':
+                stack.append('}')
+            elif len(stack) == 0 or stack[-1] != s[i]:
+                return False
+            else:
+                stack.pop()
+        return len(stack) == 0
+
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        dummy = ListNode(0)
+        prev = dummy
+        while l1 != None and l2 != None:
+            if l1.val > l2.val:
+                prev.next = l2
+                l2 = l2.next
+            else:
+                prev.next = l1
+                l1 = l1.next
+            prev = prev.next
+        if l1 != None:
+            prev.next = l1
+        else:
+            prev.next = l2
+        return dummy.next
